48e19c1ede58fe5bb667175bf919bb96ddd29492
SantiagoRojasPc <santiagodrp37@gmail.com>
2023-01-15 06:59:43 -0500

    metodo para crear tabla con requerimientos campos

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -9,5 +9,18 @@ namespace Turnos.Models
 
         }
         public DbSet<Especialidad> Especialidad {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)  // metodo protegido, no puede ser accedido ni modificado desde otro componente. Override va sobreescribir el metodo OnModelCreating con lo que hay en el nuevo metodo
+        {
+            modelBuilder.Entity<Especialidad>(entidad =>                    //en la creacion de la tabla especificamos las propiedades que necesitamos en los campos
+            {
+                entidad.ToTable("Especialidad");                            //nombre de la tabla
+                entidad.HasKey(e => e.IdEspecialidad);                      //llave principal de la tabla
+                entidad.Property(e => e.Descripcion)                        //campo de tabla      
+                .IsRequired()                                               //campo es requerido (no perminte nan)
+                .HasMaxLength(200)                                          //su tamaño maximo
+                .IsUnicode(false);
+            });
+        }
     }
 }
