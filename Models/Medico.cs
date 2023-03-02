e501eca0c51d5a89e1eeeb823ccae8dfee86981b
SantiagoRojasPc <santiagodrp37@gmail.com>
2023-03-01 19:31:09 -0500

    DataAnnotacions modelo medicco

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -8,10 +8,15 @@ namespace Turnos.Models
     {
         [Key]
         public int IdMedico { get; set; }
+        [Required (ErrorMessage ="Debe ingresar un nombre")]
         public string Nombre { get; set; }
+        [Required (ErrorMessage ="Debe ingresar un apellido")]
         public string Apellido { get; set; }
+        [Required (ErrorMessage ="Debe ingresar una direccion")]
         public string Direccion { get; set; }
+        [Required (ErrorMessage ="Debe ingresar un telefono")]
         public string Telefono { get; set; }
+        [Required (ErrorMessage ="Debe ingresar un email")]
         public string Email { get; set; }
         public DateTime HorarioAtencionDesde { get; set; }
         public DateTime HorarioAtencionHasta { get; set; }
