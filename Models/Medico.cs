b0390ba00878a57882a3752b088669a604d97714
SantiagoRojasPc <santiagodrp37@gmail.com>
2023-03-07 22:03:56 -0500

    Finalizacion de  dataAnotations de medico

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -26,8 +26,12 @@ namespace Turnos.Models
         [EmailAddress(ErrorMessage ="No es una dirección de email válida")]
         public string Email { get; set; }
         [Display(Name ="Horario desde")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString ="{0:hh:mm tt}", ApplyFormatInEditMode =true)]
         public DateTime HorarioAtencionDesde { get; set; }
         [Display(Name ="Horario hasta")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString ="{0:hh:mm tt}", ApplyFormatInEditMode =true)]
         public DateTime HorarioAtencionHasta { get; set; }
 
         public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
