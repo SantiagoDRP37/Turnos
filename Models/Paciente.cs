3058f063ffba7c4ca2e7bd849eb37df1c9ad2a88
SantiagoRojasPc <santiagodrp37@gmail.com>
2023-03-28 20:45:58 -0500

    DataAnnotation paciente

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -6,17 +6,25 @@ namespace Turnos.Models
     {
         [Key]
         public int IdPaciente{get; set;}
+
         [Required(ErrorMessage ="Debe ingresar un nombre")]
+        [Display(Name ="Nombre")]
         public string Nombre {get; set;}
+
         [Required(ErrorMessage ="Debe ingresar un apellido")]
+        [Display(Name ="Apellido")]
         public string Apellido{get; set;}
+
         [Required(ErrorMessage ="Debe ingresar una dirección")]
         [Display(Name ="Dirección")]
-        public string Direccion{get; set;}
+         public string Direccion{get; set;}
+
         [Required(ErrorMessage ="Debe ingresar un telefono")]
         [Display(Name ="Teléfono")]
         public string Telefono{get; set;}
+
         [Required(ErrorMessage ="Debe ingresar un email")]
+        [Display(Name ="Email")]
         public string Email{get; set;}
         
     }
